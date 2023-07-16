eb3794589694d9d0376cdc7dab5cb9e02bde1a74
SantiagoFK <84585956+SantiagoFK@users.noreply.github.com>
2023-07-16 18:33:11 -0300

    Added game cover as an image field of Game model. Added show all games as cards on gamelist template.

--- gameshop/games/models.py
+++ gameshop/games/models.py
@@ -9,6 +9,7 @@ class Game(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     release = models.DateField()
     #genre = models.CharField()
+    cover = models.ImageField(upload_to='covers/', blank=True)
     
     def __str__(self):
         return self.title
