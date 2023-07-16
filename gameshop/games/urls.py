1a03b8d9256eaf47a90661faf5dd7eb1d2e4b4b0
SantiagoFK <84585956+SantiagoFK@users.noreply.github.com>
2023-07-16 19:27:10 -0300

    Added details page feature for each game.

--- gameshop/games/urls.py
+++ gameshop/games/urls.py
@@ -1,8 +1,9 @@
 # Games App Urls
 
 from django.urls import path
-from .views import gamelist
+from .views import gamelist, gamedetail
 
 urlpatterns = [
     path('', gamelist, name='gamelist'),
+    path('<int:id>/', gamedetail, name='gamedetail'),
 ]
--- gameshop/games/views.py
+++ gameshop/games/views.py
@@ -1,9 +1,21 @@
 # Games App Views
 
 from django.shortcuts import render
+from django.http import Http404
+
 from .models import Game
 
 def gamelist(request):
     games = Game.objects.all()
     return render(request, 'games/gamelist.html', {'games': games})
 
+
+def gamedetail(request, id):
+    try:
+        game = Game.objects.get(id=id)
+    except Game.DoesNotExist:
+        raise Http404('No such game in the database.')
+
+    return render(request, 'games/gamedetail.html', {'game': game})
+
+# this can be written with the get_object_or_404 shortcut.
