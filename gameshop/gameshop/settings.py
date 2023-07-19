d12b9a86315b8f3a6baf4d30cac845bc285c5c6b
SantiagoFK <84585956+SantiagoFK@users.noreply.github.com>
2023-07-19 17:13:38 -0300

    templates directory changed to project level

--- gameshop/gameshop/settings.py
+++ gameshop/gameshop/settings.py
@@ -55,7 +55,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
