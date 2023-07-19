17fadfbe08acfe5bc57aa96fa2dade162a4a4ce8
SantiagoFK <84585956+SantiagoFK@users.noreply.github.com>
2023-07-19 17:04:00 -0300

    Login now using django view, installed crispy forms

--- gameshop/accounts/urls.py
+++ gameshop/accounts/urls.py
@@ -1,8 +1,11 @@
 # Accounts App Urls
 
 from django.urls import path
-from .views import user_login
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- gameshop/accounts/views.py
+++ gameshop/accounts/views.py
@@ -1,29 +1,10 @@
 # Accounts App views
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
 
-from .forms import LoginForm
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, # checks user credentials en db
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user) # sets user in current session
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
+@login_required
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
     
-    return render(request, 'accounts/login.html', {'form': form})
-
